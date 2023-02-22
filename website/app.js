1acb2ea768dff67b42cbea832b8d3078da594f85
DamarisCaballero <damariscallero011@gmail.com>
2023-02-22 12:40:20 -0600

    changes

--- website/app.js
+++ website/app.js
@@ -1,6 +1,7 @@
+const { type } = require("express/lib/response");
 
      //global variables//
-    const apiKey = "&appid=a9a2c0598720047fa9f01191e0f24ef7&units=imperial";
+    const apiKey = "4c192ee4cdf6417351e8c9e5e0e46371";
     const apiUrl = " http://localhost:4800";
 
     const zipCodeElement = document.getElementById('zip');
@@ -47,6 +48,7 @@
 
     //post data server for saving
     async function postDataServer(data) {
+        debugger
         let response = await fetch (`${apiUrl}postData`,{
             method: 'POST',
             headers: {'Content-Type': 'application/json' },
@@ -63,125 +65,26 @@
             updateUI();//update UI now
             else
             alert('Process not Successfuly');
-        }) catch(catchError);
+        }) .catch(catchError);
+
+    } catch(error){
+        catchError(error);
     }
     }
 
+    //update UI//
 
-
-
-
-
-
-
-
-
-
-
-
-/////////////////////////////////////////////////////////////////////
-    const projectData = async(data)=>{
-     try{
-        if(data.cod != 200){
-            return data;
+    async function updateUI(){
+        debugger
+        let response = await fetch(`${apiUrl}getALL`);
+        try{
+            response.json().then(data => { 
+                dateElement.innerHTML = `Date Is: ${data.date}`;
+                tempElement.innerHTML = `Temp Is: ${data.temp}`;
+                contentElement.innerHTML = `My feelings Is: ${data.content}`;
+            }).catch(catchError);
+        }catch (error) {
+            catchError(error);
         }
-        
-            const info = {
-                date: newDate,
-                temp: Math.round(data.main.temp),
-                content: feelings.value,
-                city: data.name,
-                weather: data.weather[0].description,
-                country: data.sys.country,
-             };
-             return info;
-        
-    }catch(e){
-        console.log(e);
-    }
-};
-
-
-const postData = async (url='', data={})=>{
-    const response = await fetch(url, {
-        method: 'POST',
-        credentials:"same-origin",
-        headers: {
-            'Content-Type': 'application/json'
-          },
-        body: JSON.stringify(data)
-    });
-    try {
-        const result = await response.json();
-        return result;
-    }catch (err) {
-        console.error(err);
-    }
-};
-
-
-const retrieveData = async (url) => {
-   const response = await fetch(url);
-   try{
-       const result = await response.json();
-       return result;
-   }catch (err) {
-       console.error(err);
-   }
-};
-
-const updateUI = async (info) => {
-    if(!info.message){
-        city.innerHTML = info.city + ", " + info.country;
-        weather.innerHTML = info.weather;
-        temp.innerHTML = `${info.temp}&#176`;
-        content.innerHTML = info.content? info.content  : "What do you feel &#128517;";
-        date.innerHTML = info.date;
-        message.innerHTML = "";
-        document.querySelector(".weather-info").style.opacity = "1";
-        setTimeout(async ()=> {
-        document.querySelector(".api-input").style.opacity = "1";
-            document.querySelector(".api-input").style.display="flex";
-            document.querySelector(".api-input").scrollIntoView();
-        }, 1000);
-        if(info.temp < 32){
-            document.querySelector("img").setAttribute("src", "./images/snow.png");
-        }else if(info.temp>80){
-            document.querySelector("img").setAttribute("src", "./images/hot.png");
-        }else{
-            document.querySelector("img").setAttribute("src", "https://freepngimg.com/thumb/weather/23698-6-weather-transparent-background.png");
-        }
-    }else{
-        document.querySelector(".weather-info").style.opacity = "1";
-        setTimeout(async ()=> {
-        document.querySelector(".api-input").style.opacity = "0";
-            document.querySelector(".api-input").style.display="none";
-            message.innerHTML = info.message;
-        },1000);
-   
-    }
-};
-
-generate.addEventListener("click", (e)=>{
-    e.preventDefault();
-    const madeURL = `${baseURI}${zip.value},${country.value}${key}`;
-    getData(madeURL);
-    then((data)=>{
-    projectData(data);
-    then((info)=>{
-   
-    postData("/add", info);
-    then((data)=>{
+    } 
     
-        retrieveData("/all");
-        then(data=>{
-    
-        updateUI(data);
-        });
-    });
-    });
-    
-    });
-});
-
- 
